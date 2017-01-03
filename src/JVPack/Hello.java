66e1ae5a6a598522e8f8b500ebb4fba3764d40c2
pthaks <priya.a.thakur@gmail.com>
2017-01-03 14:30:38 +0530

    First demo java prg added

---
+++ src/JVPack/Hello.java
@@ -0,0 +1,10 @@
+package JVPack;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("Setting up first JAVA program -------------- Priya");
+	}
+
+}
